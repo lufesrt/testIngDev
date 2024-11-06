7e8a7d5be9a346d50c4f51c97b15ae293cc5a74b
lufesrt <lufesrt@gmail.com>
2024-11-05 23:06:30 -0500

    se crea seeder tabla users y se realice ejecucion de migrate

---
+++ back/database/seeders/UserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin User',
+                'email' => 'admin@test.ing',
+                'email_verified_at' => Carbon::now(),
+                'password' => Hash::make('admin123'), // Contraseña encriptada
+                'remember_token' => Str::random(10),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name' => 'Regular User',
+                'email' => 'user@test.ing',
+                'email_verified_at' => Carbon::now(),
+                'password' => Hash::make('user123'), // Contraseña encriptada
+                'remember_token' => Str::random(10),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ]);
+    }
+}
